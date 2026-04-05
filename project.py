89930a683018b2bfa7b573b3438971a80ff99795
seoseonglyeol <seoseonglyeol.research@gmail.com>
2026-04-06 02:08:47 +0900

    상태

---
+++ project.py
@@ -0,0 +1,13 @@
+player={
+    "배고픔": True,
+    "현제위치": "연대앞 버스정류장"
+}
+
+environment={
+    "현재시각": 11
+}
+
+print("---주인공 상태---")
+print(f'배고픔: {player["배고픔"]}')
+print(f'현재 위치: {player["현제위치"]}')
+print(f'현재 시각: {environment["현재시각"]}시')
